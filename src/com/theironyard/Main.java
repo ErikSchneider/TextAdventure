ea122cf26f5e5bf8e3542744cb2b8a34a1e6ec26
Erik <eschneider1128@gmail.com>
2016-05-19 11:58:50 -0400

    Add command system

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     static Scanner scanner = new Scanner(System.in);
+    static Player player = new Player();
 
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome Traveler!");
@@ -12,17 +13,35 @@ public class Main {
         Player player = new Player();
 
         player.chooseName();
-
         player.chooseWeapon();
-
         player.chooseLocation();
 
         player.findItem("armor");
-
         player.findItem("potion");
 
         System.out.println(player.name);
     }
 
+    public static String nextLine() {
+        String line = scanner.nextLine();
+        while (line.startsWith("/")) {       // Does line still start with "/" ? if it does loop will continue
+            if (line.equals("/inv")) {
+                for (String item : player.items) {
+                    System.out.println(item);
+
+                }
+            }
+            else {
+                System.out.println("Command not found.");
+            }
+
+            line = scanner.nextLine();
+
+        }
+        return line;
+    }
+
 
 }
+
+
