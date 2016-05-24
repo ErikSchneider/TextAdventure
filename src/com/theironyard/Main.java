6b7109fb74aa1e8368ec2a9ebad9f94b1ed9d877
Erik <eschneider1128@gmail.com>
2016-05-24 10:49:25 -0400

    %s

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -10,8 +10,6 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome Traveler!");
 
-        Player player = new Player();
-
         player.chooseName();
         player.chooseWeapon();
         player.chooseLocation();
